02227195b3e4f3885b741dc30f9e176b11f8775e
Genz Alok <genzalok24@gmail.com>
2025-06-20 08:58:37 +0530

    owner info.py

--- info.py
+++ info.py
@@ -95,7 +95,7 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/ipopcorn_group')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/filmyrip_official')
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/genzalok')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Anonymous_rebot')
 DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/Filmyrip_official')
 OWNERID = int(os.environ.get('OWNERID', '6626499601'))  # Replace with the actual admin ID
 
