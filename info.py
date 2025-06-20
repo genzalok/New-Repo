c28c4ab26cbde316ba821a1e0516f0738c21c1d8
Genz Alok <genzalok24@gmail.com>
2025-06-20 08:58:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Deendayal_search')
 API_ID = int(environ.get('API_ID', '29236719'))
 API_HASH = environ.get('API_HASH', '1ccf1bd0a86af974e3210a55f662c062')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6897344346:AAHmKYPSYenhoQrTq4-ltGIYcOzFlsBsneo")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # ============================
 # Bot Settings Configuration
